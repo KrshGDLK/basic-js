09d62c8b377f4bc2e9be23b43e8add3c3ffe6f3b
Kirill <kiryxa561@gmail.com>
2022-10-17 22:03:53 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,26 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-
-module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  return {turns: (2 ** disksNumber - 1),
-    seconds: (Math.floor((2 ** disksNumber - 1) / turnsSpeed * 60 * 60))};
+/**
+ * Calculate turns number and time (in seconds) required
+ * to solve puzzle
+ * 
+ * @param {Number} disks number of disks
+ * @param {Number} turnsSpeed speed (in turns/hour)
+ * @return {Object} object with props turns (number of turns)
+ * and seconds (time in seconds)
+ *
+ * @example
+ * 
+ * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
+ *
+ */
+function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = 1
+  for(i = 1; i < disksNumber; i++) turns = turns * 2 + 1
+  let seconds = Math.floor( (turns * 3600) / turnsSpeed )
+  return { turns: turns, seconds: seconds }
 }
 
-
+module.exports = {
+  calculateHanoi
+};
